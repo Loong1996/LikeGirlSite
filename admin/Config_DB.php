1931611a8ca9c844988bfb5d56cae75e464e8148
Ki <5532060+kiCode111@user.noreply.gitee.com>
2022-12-22 14:40:55 +0000

    update admin/Config_DB.php.
    Signed-off-by: Ki <5532060+kiCode111@user.noreply.gitee.com>

--- admin/Config_DB.php
+++ admin/Config_DB.php
@@ -11,10 +11,10 @@ header("Content-Type:text/html; charset=utf8");
 //localhost 为数据库地址 一般用默认的即可 或（127.0.0.1）
 $db_address = "localhost";
 //数据库用户名
-$db_username = "42_192_138_177";
+$db_username = "root";
 //数据库密码
-$db_password = "rWrfXSHyirKGawtd";
+$db_password = "root";
 //数据库表名 （默认与数据库用户名相同）
-$db_name = "42_192_138_177";
+$db_name = "like";
 //敏感信息修改安全码 建议设置复杂一些
 $Like_Code = "likegirl";
